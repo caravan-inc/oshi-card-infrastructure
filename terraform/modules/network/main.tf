aebca493959384fbcc7adff70fa9d7931ae224f1
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-04-28 16:02:10 +0900

    Refactor network name

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,11 +1,11 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
+  name                    = "oshi-card-network"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
 
 resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
+  name          = "oshi-card-subnet"
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
